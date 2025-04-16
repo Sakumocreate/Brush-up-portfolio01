fc4feb4b2aa5610ae39da6f02a84bdd1f22a896c
Sakumocreate <sakumocreate@gmail.com>
2025-04-17 00:37:42 +0900

    イートインコーヒー代金自動計算機くん.java
    Progate+Youtubeの解説動画などを2日見て作成しました。

--- javaテストコード.java
+++ javaテストコード.java
@@ -1,8 +1,26 @@
+/*
+progateで学習出来た範囲と自身で勉強した部分を組み合わせて作成しております。
+ミスがあったらすみません。
+*/
 
-System.out.print ("風邪を移すのが心配です");
+class main{
+  public static void main(String[] args) {
 
-System.out.print ("私はどうしたら良いでしょうか？");
+    System.out.print("イートイン料金を自動で計算するよ！");
+    
+    int kounyukingaku = 500; //ここに購入金額を挿入
+    
+    Double zeiritu =1.08; //ここで税率を記載
+    Double daikin = kounyukingaku * zeiritu　//代金×税率
+    
+System.out.println ("あなたの購入金額は"+daikin+"円です");
 
-System.out.print("7+8");
-
-//ココにコメントを挿入してください。
+    if(daikin >=500) {
+      System.out.println("お買い上げ、ありがとうございまーす！");　//一定金額以下ならありがとうございました！
+    }else{ System.out.println("予算オーバーだ！出直して来な！！"); //一定金額以上なら詰ってくれます。
+         }
+  } 
+/*
+それと、支払い後一定確立で財布が爆発するコードを入れたかったのですが、
+まだわからなかったので入れれていません。悔しいです
+*/
