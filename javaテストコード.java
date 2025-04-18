8559e3224378cfd89a3a99c0d041d358a93f47fc
Sakumocreate <sakumocreate@gmail.com>
2025-04-18 17:06:08 +0900

    Update javaテストコード.java

--- javaテストコード.java
+++ javaテストコード.java
@@ -4,23 +4,22 @@ progateで学習出来た範囲と自身で勉強した部分を組み合わせ�
 */
 
 class main{
-  public static void main(String[] args) {
+public static void main {
 
-    System.out.print("イートイン料金を自動で計算するよ！");
-    
-    int kounyukingaku = 500; //ここに購入金額を挿入
-    
-    int zeiritu =1.08; //ここで税率を記載
-    int daikin = kounyukingaku * zeiritu　//代金×税率
-    
-System.out.println ("あなたの購入金額は"+daikin+"円です");
+System.out.print("イートイン料金を自動で計算するよ！");
 
-    if(daikin >=500) {
-      System.out.println("お買い上げ、ありがとうございまーす！");　//一定金額以下ならありがとうございました！
-    }else{ System.out.println("予算オーバーだ！出直して来な！！"); //一定金額以上なら詰ってくれます。
-         }
-  } 
+int value = 500; //ここに購入金額を挿入
+int tax =1.08; //ここで税率を記載
+int buy = value * tax//代金×税率
+
+System.out.println ("あなたの購入金額は"+buy+"円です");
+
+if(daikin >=500) {
+System.out.println("お買い上げ、ありがとうございまーす！");}//一定金額以下ならありがとうございました！
+else{ System.out.println("予算オーバーだ！出直して来な！！");}//一定金額以上なら詰ってくれます。 
+} 
 /*
 それと、支払い後一定確立で財布が爆発するコードを入れたかったのですが、
 まだわからなかったので入れれていません。悔しいです。次までには学習して組み込みたいです。
 */
+
